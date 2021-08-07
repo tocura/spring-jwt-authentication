47a04143b7f20a779df8400d135fecd2a460f84a
Mauricio Tocura <mryutoc@gmail.com>
2021-08-07 11:31:09 -0300

    User service created
    The UserService will implements UserDetailsService to find an user by
    it's login at the moment to authenticate it

---
+++ src/main/java/br/com/jwtautenthication/service/UserService.java
@@ -0,0 +1,30 @@
+package br.com.jwtautenthication.service;
+
+import br.com.jwtautenthication.exceptions.InvalidCredentialsException;
+import br.com.jwtautenthication.model.User;
+import br.com.jwtautenthication.repository.UserRepository;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@NoArgsConstructor
+public class UserService implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+        return this.userRepository
+                .findUserByLogin(login)
+                .orElseThrow(InvalidCredentialsException::new);
+    }
+}

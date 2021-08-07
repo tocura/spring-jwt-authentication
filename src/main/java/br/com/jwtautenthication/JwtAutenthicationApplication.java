d4ce0fcf1cbe47f3007e8a40beee2bfdbacc3bbf
Mauricio Tocura <mryutoc@gmail.com>
2021-08-07 10:22:02 -0300

    Initialization spring boot application

---
+++ src/main/java/br/com/jwtautenthication/JwtAutenthicationApplication.java
@@ -0,0 +1,13 @@
+package br.com.jwtautenthication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAutenthicationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtAutenthicationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/jwtautenthication/JwtAutenthicationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jwtautenthication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAutenthicationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

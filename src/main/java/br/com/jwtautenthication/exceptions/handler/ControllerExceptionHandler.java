c1536fa0f08f2950b8487bb5ca2aa2acf916e5fd
Mauricio Tocura <mryutoc@gmail.com>
2021-08-07 14:10:41 -0300

    Create handler for field validations

--- src/main/java/br/com/jwtautenthication/exceptions/handler/ControllerExceptionHandler.java
+++ src/main/java/br/com/jwtautenthication/exceptions/handler/ControllerExceptionHandler.java
@@ -1,13 +1,20 @@
 package br.com.jwtautenthication.exceptions.handler;
 
 import br.com.jwtautenthication.dtos.ExceptionDTO;
+import br.com.jwtautenthication.dtos.FieldValidationDTO;
 import br.com.jwtautenthication.exceptions.InvalidCredentialsException;
 import br.com.jwtautenthication.exceptions.InvalidTokenException;
 import br.com.jwtautenthication.exceptions.ResourceNotFoundException;
+import br.com.jwtautenthication.utils.exceptions.FieldErrorUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.List;
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
@@ -25,5 +32,15 @@ public class ControllerExceptionHandler {
     @ExceptionHandler(InvalidTokenException.class)
     public ResponseEntity<ExceptionDTO> invalidTokenHandler(InvalidTokenException e) {
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new ExceptionDTO(e.getMessage()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> argumentNotValidHandler(MethodArgumentNotValidException e) {
+        BindingResult result = e.getBindingResult();
+
+        List<FieldError> fieldErrors = result.getFieldErrors();
+        FieldValidationDTO exceptions = new FieldValidationDTO(FieldErrorUtils.processFieldErrors(fieldErrors));
+
+        return ResponseEntity.badRequest().body(exceptions);
     }
 }

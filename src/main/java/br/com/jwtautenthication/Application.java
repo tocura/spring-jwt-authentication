72bf1870fd345d21f1fdd5c0cba7d46c970f426b
Mauricio Tocura <mryutoc@gmail.com>
2021-08-07 12:06:30 -0300

    Rename application class to Application

--- src/main/java/br/com/jwtautenthication/Application.java
+++ src/main/java/br/com/jwtautenthication/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JwtAutenthicationApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(JwtAutenthicationApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }

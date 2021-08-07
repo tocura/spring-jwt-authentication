ea494f3d036c8bee46933b65714f35bbe236942b
Mauricio Tocura <mryutoc@gmail.com>
2021-08-07 14:30:37 -0300

    Create name query and implements login and sign up
    Create name query in User Repository to find an user by it's email
    
    Create method to login and sign up an user to the database

--- src/main/java/br/com/jwtautenthication/service/UserService.java
+++ src/main/java/br/com/jwtautenthication/service/UserService.java
@@ -1,12 +1,21 @@
 package br.com.jwtautenthication.service;
 
+import br.com.jwtautenthication.dtos.request.LoginRequestDTO;
+import br.com.jwtautenthication.dtos.response.LoginResponseDTO;
 import br.com.jwtautenthication.exceptions.InvalidCredentialsException;
+import br.com.jwtautenthication.exceptions.UniqueViolationException;
+import br.com.jwtautenthication.model.User;
 import br.com.jwtautenthication.repository.UserRepository;
+import br.com.jwtautenthication.utils.security.JWTUtils;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -14,10 +23,21 @@ import org.springframework.stereotype.Service;
 public class UserService implements UserDetailsService {
 
     private UserRepository userRepository;
+    private AuthenticationManager authenticationManager;
+    private BCryptPasswordEncoder encoder;
+    private JWTUtils jwtUtils;
 
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(
+            UserRepository userRepository,
+            AuthenticationManager authenticationManager,
+            BCryptPasswordEncoder encoder,
+            JWTUtils jwtUtils
+    ) {
         this.userRepository = userRepository;
+        this.authenticationManager = authenticationManager;
+        this.encoder = encoder;
+        this.jwtUtils = jwtUtils;
     }
 
     @Override
@@ -25,5 +45,38 @@ public class UserService implements UserDetailsService {
         return this.userRepository
                 .findUserByLogin(login)
                 .orElseThrow(InvalidCredentialsException::new);
+    }
+
+    public LoginResponseDTO login(LoginRequestDTO login) {
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(login.getLogin(), login.getPassword())
+            );
+        }
+        catch (BadCredentialsException e) {
+            throw new InvalidCredentialsException();
+        }
+
+        UserDetails user = loadUserByUsername(login.getLogin());
+
+        String token = this.jwtUtils.generateToken((User) user);
+
+        return new LoginResponseDTO(login.getLogin(), token);
+    }
+
+    public LoginResponseDTO signUp(User user) {
+        this.userRepository
+                .findUserByLogin(user.getLogin())
+                .orElseThrow(() -> new UniqueViolationException("Login"));
+
+        this.userRepository
+                .findUserByEmail(user.getEmail())
+                .orElseThrow(() -> new UniqueViolationException("Email"));
+
+        user.setPassword(encoder.encode(user.getPassword()));
+
+        String token = this.jwtUtils.generateToken(user);
+
+        return new LoginResponseDTO(user.getLogin(), token);
     }
 }
